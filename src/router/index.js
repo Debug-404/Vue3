38b08ac2a982c37e918f7a0a078b7d8ea964471f
Debug <1944282957@qq.com>
2022-09-06 00:46:19 +0800

    优化

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import {createRouter, createWebHistory} from "vue-router";
+import {createRouter, createWebHashHistory} from "vue-router";
 import {getToKen} from "../utils/userCookie.js";
 import {ElNotification} from "element-plus";
 
@@ -26,7 +26,7 @@ const routes = [
 ];
 
 const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHashHistory(),
     routes,
 });
 
